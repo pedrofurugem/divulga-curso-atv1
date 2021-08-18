c431dae49b6a3095b6582583f1514d3af31f62ef
Pedro <pedrofuruguem@hotmail.com>
2021-08-18 18:52:00 -0300

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CursoComponent } from './curso/curso.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule
